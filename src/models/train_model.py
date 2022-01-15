e638bb203b65dcbd9c6926fb106f1e1eeebaa46f
August Ehlers <august.ehlers@gmail.com>
2022-01-15 21:09:40 +0100

    Added sensitivity + specificity measurements

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,9 +1,12 @@
 import os
 import pprint
 import sys
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
 
 import click
 import numpy as np
+from sklearn.metrics import confusion_matrix
 import torch
 import torch.nn.functional as F
 import wandb
@@ -32,7 +35,7 @@ parameters_dict = {
 sweep_config["parameters"] = parameters_dict
 pprint.pprint(sweep_config)
 
-sweep_id = wandb.sweep(sweep_config, project="Amazon-Reviews")
+sweep_id = wandb.sweep(sweep_config, project="Amazon-Reviews", entity="amazonproject")
 
 table = wandb.Table(columns=["ReviewRating", "PredictedRating"])
 
@@ -84,6 +87,7 @@ def train(input_filepath, config=None):
         config = wandb.config
         print("Initializing training")
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        print(device)
         model = SentimentClassifier(n_classes=3, p=config.drop_out).to(device)
         train_set = torch.load(input_filepath)
         trainloader = DataLoader(train_set, batch_size=config.batch_size, shuffle=True)
@@ -101,6 +105,8 @@ def train(input_filepath, config=None):
             print(f"Epoch {i+1} of {config.epochs}")
             running_loss = 0
             running_acc = 0
+            running_sens = 0
+            running_spec = 0
             for batch_idx, data in enumerate(trainloader):
                 print(f"Batch {batch_idx+1} of {len(trainloader)}")
                 input_ids = data["input_ids"].to(device)
@@ -114,10 +120,16 @@ def train(input_filepath, config=None):
                 loss.backward()
                 optimizer.step()
 
-                y_pred = F.softmax(output, dim=1).argmax(dim=1)
-                running_acc += (
-                    ((y_pred == labels).sum() / labels.shape[0]) / len(trainloader)
-                ).item()
+                y_pred = F.softmax(output, dim=1).argmax(dim=1).data.numpy()
+                confmat = confusion_matrix(labels.data.numpy(), y_pred)
+                tp = np.trace(confmat)
+                fn = np.sum(confmat[0,1:3]) + np.sum(confmat[1,0]) + np.sum(confmat[1,2]) + np.sum(confmat[2,0:2])
+                tn = (np.sum(confmat[0:2,0:2]) + np.sum(confmat[1:3,1:3]) + confmat[0,0] + confmat[0,2] + confmat[2,0] + confmat[2,2])
+                fp = (np.sum(confmat[1:3,0]) + confmat[0,1] + confmat[2,1] + np.sum(confmat[0:2,2]))
+
+                running_sens += tp / (tp + fn) / len(trainloader)
+                running_spec += tn / (tn + fp) / len(trainloader)
+                running_acc += np.trace(confmat) /confmat.sum() / len(trainloader)
                 wandb.log(
                     {
                         "Epoch_" + str(i + 1) + " (Batch loss)": running_loss,
@@ -125,7 +137,7 @@ def train(input_filepath, config=None):
                     }
                 )
                 if ((batch_idx + 1) % 5) == 0:
-                    print(f"Loss: {running_loss} \tAccuracy.: {running_acc * 100}%")
+                    print(f"Loss: {running_loss} \tAccuracy: {round(running_acc,2) * 100}%\nSensitivity: {round(running_sens,2) * 100}%\tSpecificity: {round(running_spec,2) * 100}%")
                     random_review = np.random.randint(labels.shape[0])
                     table.add_data(labels[random_review], y_pred[random_review])
             print(
@@ -134,8 +146,10 @@ def train(input_filepath, config=None):
             wandb.log(
                 {
                     "Loss": running_loss / len(trainloader),
-                    "Accuracy (%)": running_acc,
-                    "Classes": table,
+                    "Accuracy": running_acc,
+                    "Sensitivity": running_sens,
+                    "Specificity": running_spec,
+                    "Classes": table
                 }
             )
         torch.save(
