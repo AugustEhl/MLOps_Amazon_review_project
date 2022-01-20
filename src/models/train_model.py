9367ae5dd4e084aff92a83db0bbd7264766d7304
August Ehlers <august.ehlers@gmail.com>
2022-01-20 11:41:52 +0100

    Train paralellized

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -6,13 +6,14 @@ import warnings
 warnings.filterwarnings("ignore", category=FutureWarning)
 
 import numpy as np
-from sklearn.metrics import confusion_matrix
 import torch
 import torch.nn.functional as F
 import wandb
 from torch import nn, optim
 from torch.utils.data import DataLoader
+from torch.nn.parallel import DistributedDataParallel as DDP
 from transformers import BertModel
+from torch.profiler import profile, record_function, ProfilerActivity,tensorboard_trace_handler
 
 sys.path.insert(0, os.getcwd() + "/src/data/")
 from make_dataset import AmazonData
@@ -26,10 +27,10 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 sweep_config = {"method": "random"}
 
 parameters_dict = {
-    "optimizer": {"value": "sgd"},
-    "batch_size": {"value": 20},
-    "drop_out": {"value": 0.15},
-    "lr": {"value": 0.01},
+    "optimizer": {"values": ["sgd", "adam"]},
+    "batch_size": {"values": [150,200,250]},
+    "drop_out": {"values": [0.15,0.25,0.35]},
+    "lr": {"values": [0.01,0.001]},
 }
 sweep_config["parameters"] = parameters_dict
 pprint.pprint(sweep_config)
@@ -63,7 +64,7 @@ class SentimentClassifier(nn.Module):
 loss_fn = nn.CrossEntropyLoss().to(device)
 
 def build_dataLoader(data, batch_size):
-    return DataLoader(data, batch_size=batch_size, shuffle=True, num_workers=8)
+    return DataLoader(data, batch_size=batch_size, shuffle=True, num_workers=1)
 
 def build_optimizer(opt, model, lr):
     if opt == 'adam':
@@ -81,7 +82,8 @@ def train_epoch(model, trainloader, optimizer):
         attention_mask = data["attention_mask"].to(device)
         labels = data["targets"].to(device)
         optimizer.zero_grad()
-        output = model(input_ids=input_ids, attention_mask=attention_mask)
+        with record_function("model_inference"):
+            output = model(input_ids=input_ids, attention_mask=attention_mask)
         loss = loss_fn(output, labels)
         running_loss += loss.item()
 
@@ -92,8 +94,8 @@ def train_epoch(model, trainloader, optimizer):
         running_acc += ((y_pred == labels).sum()/ labels.shape[0]).item() / num_batches
         wandb.log(
             {
-                "Epoch_" + str(i + 1) + " (Batch loss)": running_loss,
-                "Epoch_" + str(i + 1) + " (Batch accuracy)": running_acc,
+                " (Batch loss)": running_loss,
+                " (Batch accuracy)": running_acc,
             }
         )
         if ((batch_idx + 1) % 2) == 0:
@@ -127,26 +129,37 @@ def train(config=None):
         config = wandb.config
         print("Initializing training")
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        print(device)
+        print(f'Training on {device}')
         model = build_model(config.drop_out).to(device)
+        model = DDP(
+            model,
+            device_ids=[0,1]
+        )
         train_set = torch.load('data/processed/train.pth')
         trainloader = build_dataLoader(train_set, config.batch_size)
         optimizer = build_optimizer(config.optimizer, model, config.lr)
         num_epochs = 3
         model.train()
-        for i in range(num_epochs):
-            print(f"Epoch {i+1} of {num_epochs}")
-            avg_loss, avg_acc = train_epoch(model, trainloader, optimizer)
-            print(
-                f"Epoch {i+1} loss: {avg_loss} \tEpoch acc: {avg_acc}"
-            )
-            wandb.log(
-                {
-                    "Loss": avg_loss,
-                    "Accuracy": avg_acc,
-                    "Classes": table,
-                }
-            )
+        with profile(
+            activities=[ProfilerActivity.CPU],
+            record_shapes=True,
+            on_trace_ready=tensorboard_trace_handler('src/visualizations/trace_hanlder_project.pt.trace.json')
+            ) as prof:
+            for i in range(num_epochs):
+                print(f"Epoch {i+1} of {num_epochs}")
+                
+                avg_loss, avg_acc = train_epoch(model, trainloader, optimizer)
+                print(
+                    f"Epoch {i+1} loss: {avg_loss} \tEpoch acc: {avg_acc}"
+                )
+                wandb.log(
+                    {
+                        "Loss": avg_loss,
+                        "Accuracy": avg_acc,
+                        "Classes": table,
+                    }
+                )
+            prof.export_chrome_trace("trace_project.json")
         torch.save(
             model,
             f"models/model_opt{config.optimizer}_bs{config.batch_size}_do{config.drop_out}_lr{config.lr}.pth",
